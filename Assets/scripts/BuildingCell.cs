0ba673af0883fb920d6cf397df273a8c3f7fa5a7
Aleksey Averin <averin@alternativaplatform.com>
2018-02-01 00:27:57 +0300

    init project & create grid

---
+++ Assets/scripts/BuildingCell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingCell : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    public Material Material
+    {
+        get { return meshRenderer.sharedMaterial; }
+        set { meshRenderer.sharedMaterial = value; }
+    }
+
+    public bool Busy { get; set; }
+}
